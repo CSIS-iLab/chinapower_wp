59506e6064ebbaaee4fcd467172048b403318cfc
Jacque Schrag <jnschrag@gmail.com>
2017-07-31 16:43:27 -0400

    Fix podcast link on homepage

--- wp-content/themes/chinapower/template-parts/hp-latest-podcasts.php
+++ wp-content/themes/chinapower/template-parts/hp-latest-podcasts.php
@@ -7,11 +7,13 @@
  * @package chinapower
  */
 
+$permalink = esc_url(get_post_permalink(get_the_ID()));
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h3 class="entry-title"><a href="<?php esc_url( get_permalink() ); ?>" rel="bookmark"><?php the_title().chinapower_subtitle(get_the_ID()); ?></a></h3>
+		<h3 class="entry-title"><a href="<?php echo $permalink; ?>" rel="bookmark"><?php the_title().chinapower_subtitle(get_the_ID()); ?></a></h3>
 		<?php chinapower_posted_on(); ?>
 	</header><!-- .entry-header -->
 </article><!-- #post-<?php the_ID(); ?> -->
