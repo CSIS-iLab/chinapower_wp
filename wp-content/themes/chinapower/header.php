dadcfebc5b7723113c96d27281e7308518a5fbd0
Jacque Schrag <jnschrag@gmail.com>
2017-08-02 12:07:34 -0400

    wp-content/themes/chinapower/favicon.ico

--- wp-content/themes/chinapower/header.php
+++ wp-content/themes/chinapower/header.php
@@ -15,6 +15,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
 
 <?php wp_head(); ?>
 </head>
