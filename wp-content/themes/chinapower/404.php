61757689f60ac616f70b4242c16cc1474238a1e4
tuckabelle <tucker.harris121@gmail.com>
2017-08-03 19:27:34 -0400

    404 page

--- wp-content/themes/chinapower/404.php
+++ wp-content/themes/chinapower/404.php
@@ -14,46 +14,55 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'chinapower' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( 'Page not found!', 'chinapower' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'chinapower' ); ?></p>
+					<img src="/wp-content/themes/chinapower/img/ChinaPower-404-wall.jpg" alt="404 Page Not Found">
 
-					<?php
-						get_search_form();
+					<div class="content-wrapper row">
+						<div class="col-sm-6">
+							<p><?php esc_html_e( 'We were unable to locate the page you requested. The page may have moved or may no longer be available. We appologize for the inconvenience!', 'chinapower' ); ?></p>
+							<h4>Go to the <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">HOMEPAGE</a></h4>
 
-						the_widget( 'WP_Widget_Recent_Posts' );
+							<?php if(get_theme_mod('contact-email')) {
+								echo '<h4><a href="mailto:'.get_theme_mod('contact-email').'">EMAIL US</a> about this broken link</h4>';
+							} ?>
+							<?php get_search_form();?>
+						</div>
 
-						// Only show the widget if site has multiple categories.
-						if ( chinapower_categorized_blog() ) :
-					?>
+						<div class="col-sm-6">
+							<h3 class="relatedContent-heading">Recent Posts</h3>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'chinapower' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
+							<div class="relatedContent-container >">
+
+							<?php $the_query = new WP_Query( 'posts_per_page=3' ); ?>
+
+							<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+
+						<div class="relatedContent-post row">
+
+						    <div class="relatedContent-img col-xs-6">
+						    	<?php the_post_thumbnail('thumbnail'); ?>
+						    </div>
+
+						    <div class="col-xs-6">
+						    	<?php chinapower_post_categories(); ?>
+						    	<span class="relatedContent-title">
+							    	<a href="'.get_permalink().'">
+										<?php the_title(); ?> 
+									</a>
+								</span>
+						    </div>
+						</div>
+				
+						<?php 
+						endwhile;
+						wp_reset_postdata();
 						?>
-						</ul>
-					</div><!-- .widget -->
 
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'chinapower' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+					</div><!-- .content-wrapper -->
+			
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
