f73fef747bd166f1d01370b638b541df2a8d0d4c
dlousa92 <dlousa92@gmail.com>
2019-05-17 11:05:35 -0400

    add footnote shortcode to toolbar

---
+++ wp-content/plugins/toolbar-footnote/footnote.js
@@ -0,0 +1,26 @@
+(function (wp) {
+  const footnoteShortcode = function (props) {
+    return wp.element.createElement(
+      wp.editor.RichTextToolbarButton, {
+        icon: '',
+        title: 'Footnote',
+        onClick: function () {
+          props.onChange(wp.richText.insert(
+            props.value,
+            valueToInsert = '[note][/note]',
+            startIndex = props.value.start,
+            endIndex = props.value.end
+          ))
+        }
+      }
+    )
+  }
+  wp.richText.registerFormatType(
+    'footnote/sample-output', {
+      title: 'footnote Shortcode',
+      tagName: 'div',
+      className: null,
+      edit: footnoteShortcode
+    }
+  )
+})(window.wp)
---
+++ wp-content/plugins/toolbar-footnote/footnote.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Toolbar - Footnote
+*/
+
+function footnote_script_register() {
+    wp_register_script(
+        'footnote.js',
+        plugins_url( 'footnote.js', __FILE__ ),
+        array( 'wp-rich-text', 'wp-element', 'wp-editor' )
+    );
+}
+add_action( 'init', 'footnote_script_register' );
+
+function footnote_enqueue_assets_editor() {
+    wp_enqueue_script( 'footnote.js' );
+}
+add_action( 'enqueue_block_editor_assets', 'footnote_enqueue_assets_editor' );
