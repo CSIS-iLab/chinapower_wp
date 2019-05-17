91b3e414e32df46021ebf94bbf1dcc7bb0579705
dlousa92 <dlousa92@gmail.com>
2019-05-17 10:41:27 -0400

    create cpp shortcode plugin

---
+++ wp-content/plugins/toolbar-cpp/cpp.js
@@ -0,0 +1,26 @@
+(function (wp) {
+  const cppShortcode = function (props) {
+    return wp.element.createElement(
+      wp.editor.RichTextToolbarButton, {
+        icon: '',
+        title: 'CPP',
+        onClick: function () {
+          props.onChange(wp.richText.insert(
+            props.value,
+            valueToInsert = '[cpp]',
+            startIndex = props.value.start,
+            endIndex = props.value.end
+          ))
+        }
+      }
+    )
+  }
+  wp.richText.registerFormatType(
+    'cpp/sample-output', {
+      title: 'CPP Shortcode',
+      tagName: 'samp',
+      className: null,
+      edit: cppShortcode
+    }
+  )
+})(window.wp)
---
+++ wp-content/plugins/toolbar-cpp/cpp.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: CPP Icon
+*/
+
+function cpp_script_register() {
+    wp_register_script(
+        'cpp.js',
+        plugins_url( 'cpp.js', __FILE__ ),
+        array( 'wp-rich-text', 'wp-element', 'wp-editor' )
+    );
+}
+add_action( 'init', 'cpp_script_register' );
+
+function cpp_enqueue_assets_editor() {
+    wp_enqueue_script( 'cpp.js' );
+}
+add_action( 'enqueue_block_editor_assets', 'cpp_enqueue_assets_editor' );
