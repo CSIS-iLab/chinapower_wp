e9700159e2b17e8e16dd607529e7581d8b6c5c3f
Mariel de la Garza <mariel.code@gmail.com>
2020-03-16 13:54:30 -0400

    Add template for new gutenberg block and basic css

---
+++ wp-content/themes/chinapower/blocks/block-awards.php
@@ -0,0 +1,10 @@
+<div class="awards-block clearfix">
+    <div class="award-image">
+        <img src="<?php block_field('award-image'); ?>">
+    </div>
+    <div class="award-info-box">
+        <p><strong><?php block_field('award-organization'); ?></strong></p>
+        <p><?php block_field('award-name'); ?></p>
+        <p><em>category: </em><?php block_field('award-category'); ?></p>
+    </div>
+</div>
