8e106ccf10c346f554d676c0ed9874cab57c6f34
Mariel de la Garza <mariel.code@gmail.com>
2020-03-16 14:14:50 -0400

    Add missing /

--- wp-content/themes/chinapower/blocks/block-awards.php
+++ wp-content/themes/chinapower/blocks/block-awards.php
@@ -1,6 +1,6 @@
 <div class="awards-block clearfix">
     <div class="award-image">
-        <img src="<?php block_field('award-image'); ?>">
+        <img src="<?php block_field('award-image'); ?>"/>
     </div>
     <div class="award-info-box">
         <p><strong><?php block_field('award-organization'); ?></strong></p>
