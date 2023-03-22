85c7d751bdf59e189ecf8baa07b4abff6e5495fb
Mariel de la Garza <mariel.delagarza@viget.com>
2023-03-22 11:41:22 -0400

    Conditionally add guest author name to citation
    If the post type is guest_author_posts, grab the guest_author field
    from the Custom Field Types plugin and use that in the citation. Else,
    keep the original citation saying "China Power Team".

--- wp-content/themes/chinapower/inc/template-tags.php
+++ wp-content/themes/chinapower/inc/template-tags.php
@@ -236,8 +236,16 @@ function chinapower_relatedContent($rel){
 }
 
 function chinapower_citation() {
-	if ( get_the_modified_date() ) {
-		$modified_date = 'Updated ' . get_the_modified_date() . '. ';
-	}
-	return '<p class="cite-citation">China Power Team. "'.get_the_title().'" China Power. '.get_the_date().'. ' . $modified_date . 'Accessed '.current_time('F j, Y').'. '.get_the_permalink().'</p>';
+
+  $post_type = get_post_type( $post->ID );
+
+  if ( get_the_modified_date() ) {
+    $modified_date = 'Updated ' . get_the_modified_date() . '. ';
+  }
+
+  if ($post_type == 'guest_author_posts') {
+    return '<p class="cite-citation">' .get_field('guest_author'). '. "' .get_the_title(). '" China Power. '.get_the_date().'. ' . $modified_date . 'Accessed '.current_time('F j, Y').'. '.get_the_permalink().'</p>';
+  } else {
+    return '<p class="cite-citation">China Power Team. "' .get_the_title(). '" China Power. '.get_the_date().'. ' . $modified_date . 'Accessed '.current_time('F j, Y').'. '.get_the_permalink().'</p>';
+  }
 }
